51111f313fe37d062ba0775c96d8cd94f5f1f671
santiago1411 <4547@holbertonschool.com>
2022-09-09 13:52:13 -0500

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 """
+prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
     """
-    How many subs
+    Top Ten
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     header = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0 Win64x64) \
             AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.84'}
+            Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.84'
+    }
 
-    response = requests.get(url, headers=header, params={"limit": 10}, allow_redirects=False)
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=header,
+                            params=params, allow_redirects=False)
     json_response = response.json()
-    
+
     if response.status_code == 200:
         for i in json_response['data']['children']:
             print(i['data']['title'])
